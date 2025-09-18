6f242389af84b71e272e5dca61200ec5fb4dc3d7
thisisgenesis <greatfulgen@yahoo.com>
2025-09-18 19:26:26 -0400

    added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = (response.data.temperature.current);
+    let cityElement = document.querySelector("#city");
+ 
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+    
+
+}
+
+function searchCity(city) {
+    let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(refreshWeather);
+    
+
+}
+
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
     let searchFormElement = document.querySelector("#search-form");
-    searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+    
+searchCity("New York");
